20a93e8349020979687098990104261669d380c1
kimajswl <ajswl4763@naver.com>
2025-04-16 19:14:38 +0900

    first commit

---
+++ src/test/java/com/example/jugangmate/JugangmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jugangmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JugangmateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
